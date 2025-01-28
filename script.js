43f39b26840ad36b9c109cef89acf2dc1802177e
mintthiha <mintthiha@gmail.com>
2025-01-28 18:24:33 -0500

    Fix some Css

--- script.js
+++ script.js
@@ -47,18 +47,4 @@ document.addEventListener("DOMContentLoaded", () => {
   imageWrapper.addEventListener("mouseleave", () => {
     image.style.transform = "rotateX(0deg) rotateY(0deg)";
   });
-
-
-  //Lenis scrolling
-  const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  });
-
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-
-  requestAnimationFrame(raf);
 });
